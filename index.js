ee6e739a3ba2f37bee707b8d35e84ec8e28247fe
John <johntaykimani@gmail.com>
2024-01-05 15:00:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+
+// remove the DOM node main#main
+main.remove();
+
+// create a new H1 element
+
+const newHeader = document.createElement("h1");
+
+// create an id, named victory for the H1 tag
+
+newHeader.id = "victory";
+
+// add text to the H1 tag
+
+newHeader.textContent = "Kimani is the champion"
